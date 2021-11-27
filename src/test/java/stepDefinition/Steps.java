5924812f2d7a4aa053a8c404d64f8df94dc5b4c5
rajaniJune2021 <itsrajani2020@gmail.com>
2021-11-26 20:15:32 -0800

    initial commit

---
+++ src/test/java/stepDefinition/Steps.java
@@ -0,0 +1,18 @@
+package stepDefinition;
+
+import cucumber.api.java.en.Given;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+
+public class Steps {
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Given("^Rajani is stubborn$")
+    public void rajani_is_stubborn() throws Throwable {
+    }
+
+    @Given("^Rajani is younger to Naresh$")
+    public void rajani_is_younger_to_Naresh() throws Throwable {
+    }
+
+}
---
+++ src/test/resources/features/Test.feature
@@ -0,0 +1,5 @@
+Feature: Testing the allure report with BDD
+
+  Scenario: Validate generation of Allure report with BDD
+    Given Rajani is stubborn
+    And Rajani is younger to Naresh
